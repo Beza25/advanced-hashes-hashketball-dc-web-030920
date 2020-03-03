d1f7b7f0b902ff2158dc5839fcbd9abe3f57ba24
Beza Sirak <sirak.bezawit@gmail.com>
2020-03-03 14:19:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -285,16 +285,13 @@ def player_with_longest_name
       if attribute == :players
         data.each do |player|
           if player[:player_name].length > longest_name.length
-            
-            player[:player_name] 
             longest_name = player[:player_name]
-            # binding.pry
-           
           end
         end
       end
     end
   end
+  return longest_name
 end
 def long_name_steals_a_ton
 end
